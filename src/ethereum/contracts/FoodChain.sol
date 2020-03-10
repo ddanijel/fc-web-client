79b1908cb6bc4f04f40e3f4a5a7e13d96ced0c91
danijel <danijel.fon@gmail.com>
2020-03-10 19:17:17 +0100

    [fc] - smart contract - registering the producers

--- src/ethereum/contracts/FoodChain.sol
+++ src/ethereum/contracts/FoodChain.sol
@@ -2,64 +2,93 @@ pragma solidity ^0.6.3;
 
 contract FoodChain {
     address public owner;
-    mapping(string => bool) productTags;
-    address[] public producerAddresses;
+    address[] public producers;
 
     constructor() public {
         owner = msg.sender;
     }
 
-    function addProductTag() public {
-
+    function registerProducer(
+        string memory _producerName,
+        string memory _licenceNumber,
+        string memory _url,
+        string memory _certificates
+    ) public {
+        Producer newProducer = new Producer(
+            address(this),
+            msg.sender,
+            _producerName,
+            _licenceNumber,
+            _url,
+            _certificates
+        );
+        producers.push(address(newProducer));
     }
 
-    function isHashValid(string memory hash) public view returns (bool) {
-        return productTags[hash];
+    function describeFoodChain() public view returns (
+        address,
+        address[] memory
+    ) {
+        return (
+        owner,
+        producers
+        );
     }
 
+}
 
-    function registerProducer(string memory _producerName, string memory _licenceNumber, string memory _url, string memory _certificates) public {
-        Producer newProducer = new Producer(address(this), msg.sender);
-        producerAddresses.push(address(newProducer));
-    }
-
+contract Commons {
+    string defaultActions = "Action 1; Action 2; Action 3";
 }
 
 contract Producer {
     address foodChainOwner;
-    address producerAddress;
-    address[] productTags;
+    address owner;
     string producerName;
     string licenceNumber;
     string url;
-    string[] certificates;
-    string[] additionalActions;
+    string certificates;
+    string defaultActions;
+    address[] productTags;
 
     enum State {Active, Blocked}
 
-    constructor(address _foodChainOwner, address _producerAddress) public {
+    constructor(
+        address _foodChainOwner,
+        address _owner,
+        string memory _producerName,
+        string memory _licenceNumber,
+        string memory _url,
+        string memory _certificates
+    ) public {
         foodChainOwner = _foodChainOwner;
-        producerAddress = _producerAddress;
+        owner = _owner;
+        producerName = _producerName;
+        licenceNumber = _licenceNumber;
+        url = _url;
+        certificates = _certificates;
     }
 
-}
-
-contract Parameters {
-    enum Actions {Created, Locked, Inactive} // Enum
-
-    struct producerParameters {
-        address _address;
+    function describeProducer() public view returns (
+        address,
+        address,
+        string memory,
+        string memory,
+        string memory,
+        string memory,
+        string memory,
+        address[] memory
+    ) {
+        return (
+        foodChainOwner,
+        owner,
+        producerName,
+        licenceNumber,
+        url,
+        certificates,
+        defaultActions,
+        productTags
+        );
     }
-}
 
-
-contract ProductTag {
-    struct location {
-        string longitute;
-        string latitude;
-    }
-
-    address[] public previousProductTags;
-
-    string[] actions;
 }
